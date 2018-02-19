e74472bdf71683c547fed4d4721da1972b65ef88
Monte Thakkar <manthan.thakkar@gmail.com>
2018-02-19 15:45:07 -0800

    buttonview complete

--- CRNA/components/ButtonView.js
+++ CRNA/components/ButtonView.js
@@ -2,14 +2,35 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
+const BACKGROUND_COLORS = [ "#6BE387", "#387142", "#032E52", "#549091", "#EDD298", "#D6293D", "#FF9751", "#5CD6E8", "#AFFF6B", "#E8638E", "#FFE765" ];
+
 export default class ButtonView extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentColor: '#FEFBD0'
+    }
+  }
+
+  changeBackgroundColor() {
+    var color = BACKGROUND_COLORS[Math.floor(Math.random() * BACKGROUND_COLORS.length)];
+
+    this.setState({ currentColor: color })
+  }
+
   render() {
+    const { currentColor } = this.state
+
     return (
-      <View style={styles.buttonViewContainer}>
-        <Text style={styles.centeredText}>Button goes here! Click to change background color.</Text>
+      <View style={[styles.buttonViewContainer, { backgroundColor: currentColor }]}>
+        <Button title="Change background color"
+          onPress={() => this.changeBackgroundColor()}
+        />
       </View>
     );
   }
@@ -17,11 +38,9 @@ export default class ButtonView extends React.Component {
 
 const styles = StyleSheet.create({
   buttonViewContainer: {
-    backgroundColor: '#FEFBD0',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 400,
-    marginTop: 50
+    height: 400
   },
   centeredText: {
     color: '#775D6A',
